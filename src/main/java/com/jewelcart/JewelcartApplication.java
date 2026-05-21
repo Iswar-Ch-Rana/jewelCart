d9ca1f64b98fdc97b37574a682e0fe21db0922a6
Iswar Chandra Rana <ranabitu227@gmail.com>
2026-05-22 00:06:12 +0530

    feat: initial project setup with Spring Boot 3.5.14 and Docker Compose

---
+++ src/main/java/com/jewelcart/JewelcartApplication.java
@@ -0,0 +1,13 @@
+package com.jewelcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jewelcart/JewelcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewelcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
